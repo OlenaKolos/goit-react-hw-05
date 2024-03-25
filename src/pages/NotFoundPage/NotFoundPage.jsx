45dd6114813d3a7b5c5ab16aa1813059018e41b7
koloshelena <104507936+koloshelena@users.noreply.github.com>
2024-03-25 19:33:07 +0200

    add change

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,18 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// const NotFoundPage = () => {
-//   return (
-//     <>
-//       <p>Sorry, page not found!</p>
-//       <p>
-//         Start with <Link to="/">Home page</Link>
-//       </p>
-//     </>
-//   );
-// };
-
-// export default NotFoundPage;
-
 import { useLocation } from "react-router-dom";
 import css from "./NotFoundPage.module.css";
 import BackLink from "../../components/BackLink/BackLink";
